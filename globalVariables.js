892020263cab26be7ed2dc53fce990910cd57cd5
plHuan455 <huanvip455@gmail.com>
2023-02-10 23:10:46 +0700

    update white list

--- globalVariables.js
+++ globalVariables.js
@@ -1,12 +1,7 @@
 export const whiteList = [
-    'http://localhost:3000',
-    'http://localhost:8080',
-    'http://localhost:8080/',
+    'https://money-tracker-c.web.app',
     'http://localhost:5173',
     'http://127.0.0.1:8080/',
-    'https://thuongvachon.web.app',
-    "https://team-word-tc.web.app",
-    "https://teamwork-tc.web.app"
 ];
 
 export const DEMO_GROUP_TYPE = 'demo';
